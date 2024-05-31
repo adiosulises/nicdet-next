71400c74b846f666445d74e27a315f76c8d28c34
adiosulises <3adiosulises@gmail.com>
2024-05-30 23:41:19 -0700

    nav color change on scroll and slider test

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,42 +1,70 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Nav = () => {
+
+    // nav mobile
+
+
+    // cambiar bg del nav en scroll
+    const [color, setColor] = useState(false)
+    const changeColor = () => {
+        if (window.scrollY >= 90) {
+            setColor(true)
+        } else {
+            setColor(false)
+        }
+    }
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('scroll', changeColor)
+
+            return () => {
+                window.removeEventListener('scroll', changeColor)
+            }
+        }
+    }, [])
+
     return (
-        <header className="sticky top-6 z-50 font-bold">
-            <div className="absolute container mx-auto left-0 right-0 flex justify-between items-center">
-                <div>
-                    <Image
-                        src={"/nicdet-logo.svg"}
-                        width={180}
-                        height={500}
-                        alt='nicdet logo'
-                    />
-                </div>
-                <div>
-                    <nav>
-                        <ul className='flex flex-row gap-12 items-center'>
-                            <li>
-                                <Link href=''>Inicio</Link>
-                            </li>
-                            <li>
-                                <Link href=''>Nosotros</Link>
-                            </li>
-                            <li>
-                                <Link href=''>Servicios</Link>
-                            </li>
-                            <li>
-                                <Link href=''>Educaci&oacute;n</Link>
-                            </li>
-                            <li>
-                                <Link href=''>Fundaci&oacute;n</Link>
-                            </li>
-                            <li className='btn-blue'>
-                                <Link href=''>Contacto</Link>
-                            </li>
-                        </ul>
-                    </nav>
+        <header className={color ? 'navbar navbar-active' : 'navbar'}>
+            <div className={color ? "nav-bg nav-bg-active absolute mx-auto left-0 right-0 flex justify-between items-center" : "nav-bg absolute mx-auto left-0 right-0 flex justify-between items-center"}>
+                <div className='container mx-auto flex justify-between items-center'>
+                    <div>
+                        <Image
+                            src={"/nicdet-logo.svg"}
+                            width={180}
+                            height={500}
+                            alt='nicdet logo'
+                        />
+                    </div>
+                    <div>
+                        <nav>
+                            <ul className='flex flex-row gap-12 items-center'>
+                                <li>
+                                    <Link href=''>Inicio</Link>
+                                </li>
+                                <li>
+                                    <Link href=''>Nosotros</Link>
+                                </li>
+                                <li>
+                                    <Link href=''>Servicios</Link>
+                                </li>
+                                <li>
+                                    <Link href=''>Educaci&oacute;n</Link>
+                                </li>
+                                <li>
+                                    <Link href=''>Fundaci&oacute;n</Link>
+                                </li>
+                                <li className='btn-blue'>
+                                    <Link href=''>Contacto</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
         </header>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Slider from "./components/Slider";
 
 export default function Home() {
   return (
@@ -94,6 +95,7 @@ export default function Home() {
             />
           </Link>
         </div>
+        <Slider />
       </div>
     </>
   );
